79f5b61fe9c56f1d57a8ba71002290fe5d87c3a0
wipedlife <potatolivingstonei2p@gmail.com>
2021-06-06 11:34:03 +0300

    ssl to main

--- main.c
+++ main.c
@@ -2,13 +2,14 @@
 
 int main(int count, char *strings[])
 {
-    if ( count != 6 )
+    if ( count != 7 )
     {
-        printf("usage: %s <hostname> <portnum> <nick> <username> <realname>\n", strings[0]);
+        printf("usage: %s <hostname> <portnum> <nick> <username> <realname> <ssl >1/0>\n", strings[0]);
         exit(0);
     }
     if(!init_psql())abort();
-    struct IRCConnection con = OpenConnection(strings[1], atoi(strings[2]), false);
+    bool ssl = atoi(strings[6]) > 0 ? true : false;
+    struct IRCConnection con = OpenConnection(strings[1], atoi(strings[2]), ssl);
 
 
     regOnServ(con, strings[3], strings[4], strings[5]);
